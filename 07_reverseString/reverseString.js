5f89e7c53d049451b83f82ebdaea0d3ec1c565cf
tarunvyas-maglabs <tarunvyas@maglabs.in>
2025-11-24 12:07:25 +0530

    Completed Exercises 1 - 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseString = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        reverseString += str.charAt(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
